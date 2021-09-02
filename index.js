706794ca78643d1764562bc7d725a44937cb82cc
Kaycee <nwosukelechi90@gmail.com>
2021-09-02 17:22:38 +0100

    removing authentication temporarily

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const Users = Models.User;
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 //rerouting the code to connect to machine and heroku
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( 'mongodb+srv://KayceeSamuel:Starchichi00@kaycee.qkmkk.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Importing cors
 const cors = require('cors');
@@ -186,7 +186,7 @@ app.delete('/users/:Username', (req, res) => {
 });
 
 //Get all movies
-app.get('/movies', passport.authenticate('jwt', {session: false }), (req, res) => {
+app.get('/movies', function (req, res) {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
