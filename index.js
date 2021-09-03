9cd563b727f990047115f68e89f74d3b83bfb24b
Kaycee <nwosukelechi90@gmail.com>
2021-09-03 13:31:13 +0100

    amending cors

--- index.js
+++ index.js
@@ -26,18 +26,7 @@ mongoose.connect( 'mongodb+srv://KayceeSamuel:Starchichi00@kaycee.qkmkk.mongodb.
 
 //Importing cors
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ //If a specific origin isn't found on the list of allowed origins
-            let message = 'The CORs policy for this application does not allow access from origin ' + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
 
 //Importing auth.js file to the project
 let auth = require('./auth.js')(app);
